aefab24217e6f00b682339aa47aa42a163e009ed
Kelymelo <kelymeloliveira@gmail.com>
2022-02-07 12:28:43 -0300

    second commit

---
+++ src/com/dio/gft/exercicio1/Main.java
@@ -0,0 +1,7 @@
+package com.dio.gft.exercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
---
+++ src/com/dio/gft/exercicio3/Main.java
@@ -0,0 +1,7 @@
+package com.dio.gft.exercicio3;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
